09d0b42b5d01d579dc36e0911d022d8a868fc0a3
SoniaFranckel <SoniaFranckel@af791299-4527-bbf2-650b-268fa7f1fb99>
2010-12-04 23:05:16 +0000

    add tabbed activities

---
+++ trunk/USAR-Mobile/src/usar/mobile/CollectTab.java
@@ -0,0 +1,21 @@
+package usar.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CollectTab extends Activity {
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);*/
+     //   setContentView(R.layout.collecttab);
+
+        TextView textview = new TextView(this);
+        textview.setText("This is the Artists tab");
+        setContentView(textview);
+    }
+}
