c4943a24ceaa8b5a379df121b579083c4e2f5539
SoniaFranckel <SoniaFranckel@af791299-4527-bbf2-650b-268fa7f1fb99>
2010-12-17 23:34:35 +0000

    some more maps stuff, esp maps on locations tab

--- USAR-Mobile/src/usar/mobile/LocationsActivity.java
+++ USAR-Mobile/src/usar/mobile/LocationsActivity.java
@@ -1,13 +1,28 @@
 package usar.mobile;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LocationsActivity extends Activity {
+public class LocationsActivity extends MapActivity {
+	private MapView mapView;
+
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.locationstab);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.setBuiltInZoomControls(true);
+        
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
 }
--- USAR-Mobile/src/usar/mobile/MapsActivity.java
+++ USAR-Mobile/src/usar/mobile/MapsActivity.java
@@ -2,15 +2,32 @@ package usar.mobile;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
 public class MapsActivity extends MapActivity {
+	private MapView mapView;
+
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mapstab);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.setBuiltInZoomControls(true);
+        
+        Button button = (Button) findViewById(R.id.saveMap);
+        button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				saveMap();
+			}
+		});
+    }
+    private void saveMap() {
+    	//TODO(sonnyf)
     }
 
 	@Override
